fbb5f1904dec9570a153f5ecea0b8ff1311233af
taeeunkimiane <139246926+taeeunkimiane@users.noreply.github.com>
2025-06-02 19:36:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import streamlit as st
 st.title('나의 첫 웹 서비스 만들기!!')
 name = st.text_input('이름을 입력하세요 :')
 menu = st. selectbox('좋아하는 음식을 선택해 주세요:', ['망고빙수', '아몬드 봉봉'])
-if st.buttun('안사말 생성'):
+if st.button('안사말 생성'):
   st.write(name + '님! 당신이 좋아하는 음식은' + menu + '이군요? 저도 좋아요!')
